85dd17bc1f43092ad80b8fd7881b28cb5b1d6898
Steve Green <home@steve.green.com>
2023-12-31 16:10:15 +0000

    Initial Commit - Chakra UI complete

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Features } from './Features';
+import { Header } from './Header';
+import { Pricing } from './Pricing';
+
+
+function App() {
+
+  return (
+   <div className='App' >
+    <Header />
+    <Pricing />
+    <Features />
+   </div>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export const Header = () => (
+    <Box pb="28" as="section" alignItems="center">
+      <Box
+        color='gray.50'
+        bg='purple.600'
+        pt='90px'
+        pb='28'
+        px='8'
+        textAlign='center'
+    >
+    <Heading fontWeight='extrabold' fontSize={['3xl','3xl','5xl']} textAlign={['left','left','center']}>
+      Simple pricing for your business
+    </Heading>
+    <Text fontWeight='medium' fontSize={['sm','lg','2xl']} pt='4' textAlign={['left','left','center']}>
+      Plans that are carefully crafted to suit your business.
+    </Text>
+   </Box>
+  </Box>
+);
